640f76d047abb872ac2c02c4294f059af0d738b2
Yuichi Tateno <hotchpotch@gmail.com>
2009-05-09 10:27:08 +0900

    Prefs

---
+++ chrome/content/common/20-StatusBar.js
@@ -0,0 +1,17 @@
+
+var checkStatusBar = function() {
+    var v = Prefs.fotolife.get('statusbar');
+    var s = document.getElementById('hFotolife-statusBarPanel');
+    if (v) {
+        s.removeAttribute('hidden');
+    } else {
+        s.setAttribute('hidden', true);
+    }
+};
+
+Prefs.fotolife.createListener('statusbar', checkStatusBar);
+
+EventService.createListener('load', function() {
+    checkStatusBar();
+});
+
